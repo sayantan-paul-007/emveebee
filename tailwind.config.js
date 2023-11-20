2c512d1dbec23863701c4f47b75bd547373f120a
Sayantan Paul <sayantanpaul222@gmail.com>
2023-11-21 04:31:27 +0530

    tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,10 @@ tailwind.config = {
         'mobile': '429px',
         // => @media (min-width: 1280px) { ... }
       },
+      boxShadow: {
+        'neo-border': '16px 16px 0px 0px #0f0f0f',
+        'neo-border-mobile': '6px 6px 0px 0px #0f0f0f',
+      }
     }
   }
 }
