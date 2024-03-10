505ee0cb6444e08d16a0cd69bc7c998eab0d42c2
Sayantan Paul <sayantanpaul222@gmail.com>
2024-03-10 13:06:50 +0530

    last-update-001

---
+++ script.js
@@ -0,0 +1,141 @@
+const film = document.querySelector('.film-section')
+const ott = document.querySelector('.ott-section')
+const tv = document.querySelector('.tv-section')
+const audio = document.querySelector('.audio-section')
+const brand = document.querySelector('.brand-section')
+const film_button = document.querySelector('.film-btn')
+const ott_button = document.querySelector('.ott-btn')
+const tv_button = document.querySelector('.tv-btn')
+const audio_button = document.querySelector('.audio-btn')
+const brand_button = document.querySelector('.brand-btn')
+const film_button_mobile = document.querySelector('.film-btn-mobile')
+const ott_button_mobile = document.querySelector('.ott-btn-mobile')
+const tv_button_mobile = document.querySelector('.tv-btn-mobile')
+const audio_button_mobile = document.querySelector('.audio-btn-mobile')
+const brand_button_mobile = document.querySelector('.brand-btn-mobile')
+const hamicon = document.querySelector('.ham-icon')
+const hammenu = document.querySelector('.ham-menu')
+const hamclose = document.querySelector('.close')
+hamicon.addEventListener('click', function () {
+    hammenu.classList.add('show-ham-menu')
+  })
+hamclose.addEventListener('click', function () { 
+    hammenu.classList.remove('show-ham-menu')
+ })
+film_button.addEventListener('click', function(){
+    ott.classList.add("hidden");
+    film.classList.remove('hidden');
+    tv.classList.add('hidden');
+    audio.classList.add('hidden');
+    brand.classList.add('hidden');
+    film_button.classList.add('bg-neutral','text-primary')
+    ott_button.classList.remove('bg-neutral','text-primary')
+    audio_button.classList.remove('bg-neutral','text-primary')
+    tv_button.classList.remove('bg-neutral','text-primary')
+    brand_button.classList.remove('bg-neutral','text-primary')
+})
+film_button_mobile.addEventListener('click', function(){
+    ott.classList.add("hidden");
+    film.classList.remove('hidden');
+    tv.classList.add('hidden');
+    audio.classList.add('hidden');
+    brand.classList.add('hidden');
+})
+ott_button.addEventListener('click', function(){
+        ott.classList.remove("hidden");
+        film.classList.add('hidden');
+        tv.classList.add('hidden');
+        audio.classList.add('hidden');
+        brand.classList.add('hidden');
+        film_button.classList.remove('bg-neutral','text-primary')
+        film_button.classList.add('text-neutral')
+    ott_button.classList.add('bg-neutral','text-primary')
+    audio_button.classList.remove('bg-neutral','text-primary')
+    tv_button.classList.remove('bg-neutral','text-primary')
+    brand_button.classList.remove('bg-neutral','text-primary')
+})
+ott_button_mobile.addEventListener('click', function(){
+    ott.classList.remove("hidden");
+    film.classList.add('hidden');
+    tv.classList.add('hidden');
+    audio.classList.add('hidden');
+    brand.classList.add('hidden');
+})
+tv_button.addEventListener('click', function(){
+    ott.classList.add("hidden");
+    film.classList.add('hidden');
+    tv.classList.remove('hidden');
+    audio.classList.add('hidden');
+    brand.classList.add('hidden');
+    film_button.classList.remove('bg-neutral','text-primary')
+    film_button.classList.add('text-neutral')
+    ott_button.classList.remove('bg-neutral','text-primary')
+    audio_button.classList.remove('bg-neutral','text-primary')
+    tv_button.classList.add('bg-neutral','text-primary')
+    brand_button.classList.remove('bg-neutral','text-primary')
+})
+tv_button_mobile.addEventListener('click', function(){
+    ott.classList.add("hidden");
+    film.classList.add('hidden');
+    tv.classList.remove('hidden');
+    audio.classList.add('hidden');
+    brand.classList.add('hidden');
+})
+audio_button.addEventListener('click', function(){
+    ott.classList.add("hidden");
+    film.classList.add('hidden');
+    tv.classList.add('hidden');
+    audio.classList.remove('hidden');
+    brand.classList.add('hidden');
+    film_button.classList.remove('bg-neutral','text-primary')
+    film_button.classList.add('text-neutral')
+    ott_button.classList.remove('bg-neutral','text-primary')
+    audio_button.classList.add('bg-neutral','text-primary')
+    tv_button.classList.remove('bg-neutral','text-primary')
+    brand_button.classList.remove('bg-neutral','text-primary')
+})
+audio_button_mobile.addEventListener('click', function(){
+    ott.classList.add("hidden");
+    film.classList.add('hidden');
+    tv.classList.add('hidden');
+    audio.classList.remove('hidden');
+    brand.classList.add('hidden');
+})
+brand_button.addEventListener('click', function(){
+    ott.classList.add("hidden");
+    film.classList.add('hidden');
+    tv.classList.add('hidden');
+    audio.classList.add('hidden');
+    brand.classList.remove('hidden');
+    film_button.classList.remove('bg-neutral','text-primary')
+    film_button.classList.add('text-neutral')
+    ott_button.classList.remove('bg-neutral','text-primary')
+    audio_button.classList.remove('bg-neutral','text-primary')
+    tv_button.classList.remove('bg-neutral','text-primary')
+    brand_button.classList.add('bg-neutral','text-primary')
+})
+brand_button_mobile.addEventListener('click', function(){
+    ott.classList.add("hidden");
+    film.classList.add('hidden');
+    tv.classList.add('hidden');
+    audio.classList.add('hidden');
+    brand.classList.remove('hidden');
+    film_button.classList.remove('bg-neutral','text-primary')
+    film_button.classList.add('text-neutral')
+    ott_button.classList.remove('bg-neutral','text-primary')
+    audio_button.classList.remove('bg-neutral','text-primary')
+    tv_button.classList.remove('bg-neutral','text-primary')
+    brand_button.classList.add('bg-neutral','text-primary')
+})
+function toggleDropdown() {
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+  }
+  
+  function selectOption(option) {
+    const dropdownToggle = document.querySelector('.dropdown-toggle');
+    dropdownToggle.textContent = option;
+    toggleDropdown();
+    // Additional logic for handling the selected option
+  }
+  
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+      'primary': "#0f0f0f",
+      "primary-gray": "#404040",
+      'gray': "#7f7f7f",
+      "light-gray": "#bfbfbf",
+      'neutral': "#ffffff",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      screens: {
+        'tablet': '1025px',
+        'laptop': '1600px',
+        'mobile': '429px',
+        },
+      boxShadow: {
+       
+              'neo-border': '16px 16px 0px 0px #0f0f0f',
+              'neo-border-mid': '8px 8px 0px 0px #0f0f0f',
+              'neo-border-mobile': '6px 6px 0px 0px #0f0f0f',
+              'neo-border-mobile-small': '4px 4px 0px 0px #0f0f0f',      
+      }
+    }
+  }
+}
